c6970396b19c2aa653e80f2e3cf17fb8d58b3404
Doug Berry <drberry@gmail.com>
2016-06-03 12:37:18 -0500

    Changed area finding algorithm to maximize minimum edge length instead of area

--- tests/PixTestTiming.cc
+++ tests/PixTestTiming.cc
@@ -758,7 +758,6 @@ pair <int, int> PixTestTiming::getGoodRegion(TH2D* hist, int hits) {
   if (hist->GetEntries()==0) return make_pair(0,0);
 
   int MaxGoodRegionSize=0;
-  int MaxGoodRegionArea=0;
   int GoodROCDelay=0;
   for (int startbinx=1; startbinx<=hist->GetNbinsX(); startbinx++) {
     for (int startbiny=1; startbiny<=hist->GetNbinsY(); startbiny++) {
@@ -773,8 +772,10 @@ pair <int, int> PixTestTiming::getGoodRegion(TH2D* hist, int hits) {
               if (int(hist->GetBinContent(checkbinx,checkbiny))!=hits) regiongood=false;
             }
           }
-          if (regiongood && (xsize+1)*(ysize+1)>MaxGoodRegionArea) {
-            MaxGoodRegionArea = (xsize+1)*(ysize+1);
+          int GoodRegionSize=0;
+          if (xsize >= ysize) GoodRegionSize=ysize+1;
+          else if (xsize < ysize) GoodRegionSize=xsize+1;
+          if (regiongood && GoodRegionSize>MaxGoodRegionSize) {
             if (xsize >= ysize) MaxGoodRegionSize=ysize+1;
             else if (xsize < ysize) MaxGoodRegionSize=xsize+1;
             GoodROCDelay = (startbinx-1+xsize/2)%8 | (startbiny-1+ysize/2)%8<<3;
